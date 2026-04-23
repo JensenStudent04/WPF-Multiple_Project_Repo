c90d9b9351fbeb33e05e6fd82b09dd398d6d169e
JensenStudent04 <ST10442661@myemeris.edu.za>
2026-04-23 15:39:34 +0200

    First Commit

---
+++ Repo_Test/Repo_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Repo_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Test
+        }
+    }
+}
